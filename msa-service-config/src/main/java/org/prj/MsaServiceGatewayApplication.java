9a92fe04b9eb4ef4deb88e7959cf84caa1639d7b
Vitamindragon <ghwjdgks@gmail.com>
2024-11-02 13:43:22 +0900

    feat: Config Server적용
    - Config Server적용

--- msa-service-config/src/main/java/org/prj/MsaServiceGatewayApplication.java
+++ msa-service-config/src/main/java/org/prj/MsaServiceGatewayApplication.java
@@ -5,14 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-
 @EnableConfigServer
-@EnableDiscoveryClient
 @SpringBootApplication
-public class MsaServiceConfigApplication {
+public class MsaServiceGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsaServiceConfigApplication.class, args);
+        SpringApplication.run(MsaServiceGatewayApplication.class, args);
     }
 
 }
